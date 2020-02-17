35c239aa581d0ec9f5a9b2b26f0975496d2428de
Connor Bovino <connorbovino@Connors-MacBook-Pro-3.local>
2020-02-17 10:05:26 -0500

    Local Changes

--- src/extension.ts
+++ src/extension.ts
@@ -4,61 +4,61 @@ import * as vscode from 'vscode';
 import { stringify } from 'querystring';
 
 
-const fs = require("fs");
-const parseExtract = require("./modules/parseExtract.js");
-const { OutlineProvider } = require("./modules/OutlineProvider.js");
+const fs=require("fs");
+const parseExtract=require("./modules/parseExtract.js");
+const { OutlineProvider }=require("./modules/OutlineProvider.js");
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "trinity" is now active!');
- 
-  const tChannel = vscode.window.createOutputChannel('trinity');
 
-  const OP = new OutlineProvider();
+  const tChannel=vscode.window.createOutputChannel('trinity');
+
+  const OP=new OutlineProvider();
   vscode.window.registerTreeDataProvider("package-dependencies", OP);
 
 
-  let newThing = vscode.commands.registerCommand('package-dependencies.executeTask', task => {
+  let newThing=vscode.commands.registerCommand('package-dependencies.executeTask', task => {
     vscode.tasks.executeTask(task).then(function (value) {
-        return value;
-    }, function(e) {
-        console.error('Error');
+      return value;
+    }, function (e) {
+      console.error('Error');
     });
   });
 
   context.subscriptions.push(newThing);
 
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable=vscode.commands.registerCommand('extension.helloWorld', () => {
+    // The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World! This is trinity!');
+    // Display a message box to the user
+    vscode.window.showInformationMessage('Hello World! This is trinity!');
   });
-  
+
   context.subscriptions.push(disposable);
   // In the context of this extension-
-    // On a Save of a text document in a workspace (which is an event listener)
-      // We pass the event into handle save
+  // On a Save of a text document in a workspace (which is an event listener)
+  // We pass the event into handle save
   context.subscriptions.push(vscode.workspace.onDidSaveTextDocument(event => handleSave(event)));
- 
+
 }
 
 // We recieve the event from the file that is being saved in the onDidSavetextDocument listener
-function handleSave(event: vscode.TextDocument){
+function handleSave(event: vscode.TextDocument) {
 
- // console logging and reading the file that we have saved and converting it to string 
-const result = parseExtract(fs.readFileSync(event.fileName).toString());
+  // console logging and reading the file that we have saved and converting it to string 
+  const result=parseExtract(fs.readFileSync(event.fileName).toString());
 
-console.log(result);
+  console.log(result);
 
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
--- src/modules/OutlineProvider.ts
+++ src/modules/OutlineProvider.ts
@@ -1,57 +1,120 @@
 
 import * as vscode from 'vscode';
 
-const { getGraphStructure } = require('../server/neo4j/graphdb-outline-ts');
+const { getGraphStructure }=require('../server/neo4j/graphdb-outline-ts');
 
 export class OutlineProvider implements vscode.TreeDataProvider<TreeTask> {
-  
-    constructor(private context: vscode.ExtensionContext) {
-  
-    }
 
-   public async getChildren(task?: TreeTask): Promise<TreeTask[]> {
+  onDidChangeTreeData?: vscode.Event<TreeTask|null|undefined>|undefined;
 
-      let treeTasks: TreeTask[] = [];
+  data: TreeTask[];
 
-      const dbAddress: string = "bolt://localhost";
-      const username: string = "neo4j";
-      const password: string = "test";
+  constructor(private context: vscode.ExtensionContext) {
 
-      getGraphStructure(dbAddress, username, password).then(result => {
-        console.log( "Here" ,JSON.stringify(result, null, 2));
+    //must push property keys into array
+    //Labels
+    //Movie
+    //title
+    //tagline
+    //releaseDate
+
+    //Relationships
+    //Person
+    //ActedIn
+    //Movie
+    //Directed
+    //nothaMovie  
+
+    // new TreeTask('label', Labelsdata)
+
+    // labelsdata = [ new Treetask()]
+
+    // new tree('relationship', relationshipdata)
+
+    // this.data=[new TreeTask('cars', [
+    //   new TreeTask(
+    //     'Ford', [new TreeTask('Fiesta'), new TreeTask('Focus'), new TreeTask('Mustang')]),
+    //   new TreeTask(
+    //     'BMW', [new TreeTask('320'), new TreeTask('X3'), new TreeTask('X5')])
+    // ])];
+
+    const dbAddress: string="bolt://localhost";
+    const username: string="neo4j";
+    const password: string="test";
+
+    getGraphStructure(dbAddress, username, password).then(result => {
+
+      // grab the specific categories
+      // and push them ontop tree tas
+      const resultData=result;
+
+      let labels=[];
+      resultData.graphOutline.forEach(element => {
+
+        let propertyData=[];
+
+        element.properties.forEach(element => {
+          propertyData.push(new TreeTask(element));
+        });
+
+        let newTask=new TreeTask(element.label, propertyData);
+
+        labels.push(newTask);
       });
 
+      let labelTask=new TreeTask("Label", labels);
 
-      //creating tree tasks
+      this.data=[];
 
-      treeTasks.push(new TreeTask('test1', "test2" ,vscode.TreeItemCollapsibleState.None));
-      treeTasks.push(new TreeTask('Test1', "DOG" ,vscode.TreeItemCollapsibleState.None));
-
+      this.data.push(labelTask);
 
 
-      return treeTasks;
+      //unidirectional relationships
+
+      // resultData.uniDirectionalRelationships.forEach(element => {
+
+      //   let uniData=[];
+
+      //   element.originNode;
+
+      // });
+
+
+
+
+
+      //bidirectional relationships
+
+
+
+
+    });
+
+  }
+
+  getChildren(element?: TreeTask|undefined): vscode.ProviderResult<TreeTask[]> {
+
+    if (element===undefined) {
+      return this.data;
     }
-  
-    getTreeItem(task: TreeTask): vscode.TreeItem {
-      return task;
-    }
+    return element.children;
+  }
+
+  getTreeItem(element: TreeTask): vscode.TreeItem|Thenable<vscode.TreeItem> {
+    return element;
+  }
 
 }
 
 
 class TreeTask extends vscode.TreeItem {
+  children: TreeTask[]|undefined;
 
-  type: string;
-  
-  constructor(
-      type: string, 
-      label: string, 
-      collapsibleState: vscode.TreeItemCollapsibleState,
-      command?: vscode.Command
-  ) {
-      super(label, collapsibleState);
-      this.type = type;
-      this.command = command;
+  constructor(label: string, children?: TreeTask[]) {
+    super(
+      label,
+      children===undefined? vscode.TreeItemCollapsibleState.None:
+        vscode.TreeItemCollapsibleState.Expanded);
+    this.children=children;
   }
-   
 }
