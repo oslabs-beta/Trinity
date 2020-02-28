3e953209c1e8ac8976544cd54d2aec79570d74b0
Alex Drew <alex@alexdrew.io>
2020-02-28 15:53:09 -0500

    Wired refresh button for OutlineProvider to update graph outline
    Co-authored-by: Connor Bovino <connor.bovino@gmail.com>
    Co-authored-by: Alex Bednarek <alexbednarek4@gmail.com>
    Co-authored-by: Olena Danykh <olenadanykh@gmail.com>

--- src/modules/OutlineProvider.ts
+++ src/modules/OutlineProvider.ts
@@ -30,19 +30,28 @@ interface Props {
 }
 import * as path from "path";
 
-export class OutlineProvider implements vscode.TreeDataProvider<TreeTask> {
-  onDidChangeTreeData?: vscode.Event<TreeTask | null | undefined> | undefined;
+export class OutlineProvider implements vscode.TreeDataProvider<number> {
+  //<TreeTask> {
+  // onDidChangeTreeData?: vscode.Event<TreeTask | null | undefined> | undefined;
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    number | null
+  > = new vscode.EventEmitter<number | null>();
+  readonly onDidChangeTreeData: vscode.Event<number | null> = this
+    ._onDidChangeTreeData.event;
 
   // Given Type from VSCode Extension
   data: TreeTask[];
 
   constructor(private context: vscode.ExtensionContext) {
-    this.data = this.createGraphStructure();
+    // this.data =
+    this.createGraphStructure();
   }
 
   createResultObj(resultData: GraphStructure) {
     // Create Result Obj
     const resultObj: ResultObject = {};
+
+    // console.log("CRO, Input:", resultData);
 
     for (let i = 0; i < resultData.uniDirectionalRelationship.length; i += 1) {
       // for each unique  item in origin array, create a key on result obj
@@ -86,8 +95,16 @@ export class OutlineProvider implements vscode.TreeDataProvider<TreeTask> {
       let nameNode: string = resultData.graphOutline[q].label;
       let propsNode: string[] = resultData.graphOutline[q].properties;
 
-      if (resultObj.nameNode) {
-        resultObj.nameNode.Properties = propsNode;
+      if (!resultObj[nameNode]) {
+        resultObj[nameNode] = {
+          "Uni-Directional": {},
+          "Bi-Directional": {},
+          Properties: []
+        };
+      }
+
+      if (resultObj[nameNode]) {
+        resultObj[nameNode].Properties = propsNode;
       }
     }
 
@@ -100,16 +117,22 @@ export class OutlineProvider implements vscode.TreeDataProvider<TreeTask> {
     const username: string = "neo4j";
     const password: string = "test";
 
-    return getGraphStructure(dbAddress, username, password).then(
+    const boundGetGraphStructure = getGraphStructure.bind(this);
+
+    boundGetGraphStructure(dbAddress, username, password).then(
       (res: GraphStructure | undefined) => {
         if (res !== undefined) {
           //Saves the structure of ResultObject- Type is defined in Result Object
-          let newData = [];
+          // let newData = [];
           const resultObject: ResultObject = this.createResultObj(res);
-          newData = this.setUpData(resultObject);
+
+          console.log(resultObject);
+
+          this.data = this.setUpData(resultObject);
+          this._onDidChangeTreeData.fire();
           // console.log("inside promise", newData);
           // this.data = newData;
-          return newData;
+          // return newData;
         }
       }
     );
@@ -169,121 +192,6 @@ export class OutlineProvider implements vscode.TreeDataProvider<TreeTask> {
   getTreeItem(element: TreeTask): vscode.TreeItem | Thenable<vscode.TreeItem> {
     return element;
   }
-
-  // refresh(offset?: number): void {
-  //   vscode.window.showInformationMessage("Refresh Triggered");
-  //   console.log("refresh triggered");
-  //   // this.parseTree();
-  //   // if (offset) {
-  //   // 	this._onDidChangeTreeData.fire(offset);
-  //   // } else {
-  //   // 	this._onDidChangeTreeData.fire();
-  //   // }
-  //   const dbAddress: string = "bolt://localhost";
-  //   const username: string = "neo4j";
-  //   const password: string = "test";
-
-  //   getGraphStructure(dbAddress, username, password).then(result => {
-  //     // all data
-  //     const resultData = result;
-
-  //     // interface
-  //     interface LooseObject {
-  //       [key: string]: any;
-  //     }
-
-  //     // Create Result Obj
-
-  //     const resultObj: LooseObject = {};
-
-  //     for (
-  //       let i = 0;
-  //       i < resultData.uniDirectionalRelationship.length;
-  //       i += 1
-  //     ) {
-  //       // for each unique  item in origin array, create a key on result obj
-  //       let originNodes = resultData.uniDirectionalRelationship[i].originNode;
-  //       let uniRelation = resultData.uniDirectionalRelationship[i].relationship;
-  //       let dependentNode =
-  //         resultData.uniDirectionalRelationship[i].dependentNode;
-  //       for (let x = 0; x < originNodes.length; x += 1) {
-  //         if (!resultObj[originNodes[x]]) {
-  //           resultObj[originNodes[x]] = {
-  //             "Uni-Directional": {}
-  //           };
-  //         }
-  //         resultObj[originNodes[x]]["Uni-Directional"][
-  //           uniRelation
-  //         ] = dependentNode;
-  //       }
-  //     }
-  //     // For all the bidrectional relationships in the data
-  //     for (let y = 0; y < resultData.biDirectionalRelationship.length; y += 1) {
-  //       // Array of all the origin nodes in relationship object
-  //       let originNodes = resultData.biDirectionalRelationship[y].originNode;
-  //       // Relationship between origin nodes and depenent node
-  //       let biRelation = resultData.biDirectionalRelationship[y].relationship;
-  //       // Array of depend Nodes
-  //       let dependentNode =
-  //         resultData.biDirectionalRelationship[y].dependentNode;
-  //       // for each origin node
-  //       for (let z = 0; z < originNodes.length; z += 1) {
-  //         // We check if the result obj has that origin node
-  //         if (!resultObj[originNodes[z]]) {
-  //           resultObj[originNodes[x]] = {
-  //             "Bi-Directional": {}
-  //           };
-  //         }
-  //         // if the node is on the result obj, check if the bi relationship is on the node
-  //         if (!resultObj[originNodes[z]]["Bi-Directional"]) {
-  //           resultObj[originNodes[z]]["Bi-Directional"] = {};
-  //         }
-  //         resultObj[originNodes[z]].bi[biRelation] = dependentNode;
-  //       }
-  //     }
-
-  //     resultData.graphOutline.forEach((element: LooseObject) => {
-  //       if (!resultObj[element.label]) {
-  //         resultObj[element.label] = {};
-  //       }
-
-  //       resultObj[element.label]["Properties"] = element.properties;
-  //     });
-
-  //     this.data = [];
-
-  //     Object.keys(resultObj).forEach(element => {
-  //       let elementArray = [];
-
-  //       Object.keys(resultObj[element]).forEach(innerEl => {
-  //         let innerElArray = [];
-
-  //         // check if person.uni or properties etc is an array
-  //         if (Array.isArray(resultObj[element][innerEl])) {
-  //           resultObj[element][innerEl].forEach(innerMostEL => {
-  //             innerElArray.push(new TreeTask(innerMostEL));
-  //           });
-  //         } else {
-  //           Object.keys(resultObj[element][innerEl]).forEach(newInnerEl => {
-  //             let dependents = [];
-
-  //             resultObj[element][innerEl][newInnerEl].forEach(el => {
-  //               dependents.push(new TreeTask(el));
-  //             });
-
-  //             innerElArray.push(new TreeTask(newInnerEl, dependents));
-  //           });
-  //         }
-  //         let innerTreeTask = new TreeTask(innerEl, innerElArray);
-
-  //         elementArray.push(innerTreeTask);
-  //       });
-
-  //       let elementTreeTask = new TreeTask(element, elementArray);
-  //       this.data.push(elementTreeTask);
-  //     });
-  //   });
-  // }
 }
 
 class TreeTask extends vscode.TreeItem {
