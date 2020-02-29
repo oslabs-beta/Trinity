870c45658b3d1dcf00f7c2aa2f535ea646c213ce
alexbednarek4 <alexbednarek4@gmail.com>
2020-02-29 15:06:05 -0500

    Added testing of parseExtract and Extract functions

--- src/modules/parseExtract.ts
+++ src/modules/parseExtract.ts
@@ -1,32 +1,32 @@
-function parseExtract(string: string): Array<string|boolean> {
+function parseExtract(string: string): Array<string | boolean> {
   // Array for storing the Queries inside the string
-  let array: Array<string|boolean>=[];
+  let array: Array<string | boolean> = [];
 
   // Find queries inside of string, push them on to array
-  const findQuery=(string: string) => {
+  const findQuery = (string: string) => {
     // find the location of trinity in the string
-    const tlocation: number=string.search("Trinity");
+    const tlocation: number = string.search("Trinity");
     // console.log(tlocation);
 
     // if 0, Trinity hasn't been typed into the string
-    if (tlocation!==-1) {
+    if (tlocation !== -1) {
       // Create new string, which will be result of slicing from 'T' in Trinity
-      let newString: string=string.slice(tlocation, string.length);
+      let newString: string = string.slice(tlocation, string.length);
       // console.log(newString);
 
       //Create new variable for the query and end location
       //extract(newString) returns an array
       let queryNLocation: {
-        queryString: string|boolean;
+        queryString: string | boolean;
         currIndex: number;
-      }=extract(newString);
+      } = extract(newString);
 
       // push the query onto the main array
       array.push(queryNLocation.queryString);
 
       // recursively call the function find Query on the new string based on the location
-      let remainingString: string=newString.slice(
-        queryNLocation.currIndex+1
+      let remainingString: string = newString.slice(
+        queryNLocation.currIndex + 1
       );
 
       // recursively call the function on the new string
@@ -36,19 +36,15 @@ function parseExtract(string: string): Array<string|boolean> {
 
   //passing newString in to extract
 
-
   findQuery(string);
 
   return array;
 }
 
-
-
-const extract=(
+const extract = (
   string: string
-): { queryString: string|boolean; currIndex: number } => {
-
-  const brackets: { [key: string]: string }={
+): { queryString: string | boolean; currIndex: number } => {
+  const brackets: { [key: string]: string } = {
     "[": "PUSH TO STACK",
     "{": "PUSH TO STACK",
     "(": "PUSH TO STACK",
@@ -61,51 +57,51 @@ const extract=(
   };
 
   // extract the query
-  let stack: Array<string>=[string[8]];
-  let currIndex: number=9;
-  let queryString: string|boolean="";
+  let stack: Array<string> = [string[8]];
+  let currIndex: number = 9;
+  let queryString: string | boolean = "";
 
   // iterate until empty the stack, or find incorrect brackets
-  while (stack.length>0) {
+  while (stack.length > 0) {
     // store current char
-    const currChar: string=string[currIndex];
+    const currChar: string = string[currIndex];
     // store the brack[curChar]
-    const bracket: string|undefined=brackets[currChar];
+    const bracket: string | undefined = brackets[currChar];
     // If the variable in brackets obj
     // CASE: Not a bracket => Go to next loop
     if (!bracket) {
-      queryString+=currChar;
-      currIndex+=1;
+      queryString += currChar;
+      currIndex += 1;
       continue;
     }
     // CASE: PUSH TO STACK => Push it to the stack
-    else if (bracket==="PUSH TO STACK") {
+    else if (bracket === "PUSH TO STACK") {
       stack.push(currChar);
     }
     // CASE: Else => pop it
     // CASE: currChar is quote and top of stack is not the same quote -> push it to stack
-    else if (currChar==='"'||currChar==="'"||currChar==="`") {
+    else if (currChar === '"' || currChar === "'" || currChar === "`") {
       // CASE: currChar is quote and top of stack is the same quote -> pop from stack
-      if (stack[stack.length-1]===currChar) {
+      if (stack[stack.length - 1] === currChar) {
         stack.pop();
       } else {
         stack.push(currChar);
       }
     }
     // CASE: Closing Bracket & compliments top of stack -> pop from stack
-    else if (currChar==="]"||currChar==="}"||currChar===")") {
-      if (stack[stack.length-1]===bracket) {
+    else if (currChar === "]" || currChar === "}" || currChar === ")") {
+      if (stack[stack.length - 1] === bracket) {
         stack.pop();
       } else {
         // CASE: Closing Bracket &  does not compliment top of stack -> return false and curr location
-        queryString=false;
+        queryString = false;
         break;
       }
     }
     // prepare for next iteration
-    currIndex+=1;
-    if (stack.length>0) {
-      queryString+=currChar;
+    currIndex += 1;
+    if (stack.length > 0) {
+      queryString += currChar;
     }
   }
 
@@ -115,6 +111,4 @@ const extract=(
   };
 };
 
-
-
-module.exports={ parseExtract, extract };
+module.exports = { parseExtract, extract };
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,34 @@
-import * as assert from 'assert';
+import * as assert from "assert";
+const { parseExtract } = require("../../modules/parseExtract");
+const { extract } = require("../../modules/parseExtract");
+const { OutlineProvier } = require("../../modules/OutlineProvier")
+//import { expect } from "chai";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 // import * as myExtension from '../extension';
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+suite("Extension Test Suite", () => {
+  vscode.window.showInformationMessage("Start all tests.");
 
-	test('Sample test', () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
-	});
+  test("Sample test", () => {
+    assert.equal(-1, [1, 2, 3].indexOf(5));
+    assert.equal(-1, [1, 2, 3].indexOf(0));
+  });
+
+  test("Extract functionality", () => {
+    const extractObj = extract("Trinity('Test') stuff");
+    assert.equal(extractObj.queryString, "Test");
+    assert.equal(extractObj.currIndex, 14);
+  });
+
+  test("parseExtract functionality", () => {
+    const queryArray = parseExtract(
+      "Trinity('Test') stuff Trinity('anotherTest')"
+    );
+    assert.equal(queryArray[0], "Test");
+    assert.equal(queryArray[1], "anotherTest");
+  });
+  //Testing OutlineProvier methods
 });
